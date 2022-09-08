61ae478b53d1c22995edd2ecfba4b52842680a94
Captluke2328 <81543946+Captluke2328@users.noreply.github.com>
2022-09-09 03:21:53 +0800

    Update Read_PPM_Signal_from_FSiA6B_Using_Arduino.ino

--- Read_PPM_Signal_from_FSiA6B_Using_Arduino.ino
+++ Read_PPM_Signal_from_FSiA6B_Using_Arduino.ino
@@ -1,11 +1,11 @@
 #define channumber 6 //How many channels have your radio?
 int channel[channumber]; //read Channel values
-int PPMin = 4;
+int PPMin = 3;
 
 void setup()
 {
   Serial.begin(9600); //Start serial
-  pinMode(PPMin, INPUT); // Pin 4 as input
+  pinMode(PPMin, INPUT); // Pin 3 as input
 }
 
 void loop()
@@ -15,15 +15,28 @@ void loop()
   {
     for(int i = 1; i <= channumber; i++) //Read the pulses of the remainig channels
     {
- channel[i-1]=pulseIn(PPMin, HIGH);
+      channel[i-1]=pulseIn(PPMin, HIGH);
     }
-    for(int i = 1; i <= channumber; i++) //Prints all the values readed
-    {
- Serial.print("CH"); //Channel
- Serial.print(i); //Channel number
- Serial.print(": "); 
- Serial.println(channel[i-1]); //Print the value
-    }
-    delay(200);//Give time to print values.
+    // for(int i = 1; i <= channumber; i++) //Prints all the values readed
+    // {
+    //   Serial.print("CH"); //Channel
+    //   Serial.print(i); //Channel number
+    //   Serial.print(": "); 
+    //   Serial.println(channel[i-1]); //Print the value
+    // }
+      Serial.print("Ch1: ");
+      Serial.print(channel[1-1]);
+      Serial.print(" | Ch2: ");
+      Serial.print(channel[2-1]);
+      Serial.print(" | Ch3: ");
+      Serial.print(channel[3-1]);
+      Serial.print(" | Ch4: ");
+      Serial.print(channel[4-1]);
+      Serial.print(" | Ch5: ");
+      Serial.print(channel[5-1]);
+      Serial.print(" | Ch6: ");
+      Serial.println(channel[6-1]);
+
+    // delay(200);//Give time to print values.
   }
 }
